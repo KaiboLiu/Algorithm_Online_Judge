084bb818b68c1fa69116349f76061e563dff9657
Kaibo Liu <liukaibo87@gmail.com>
2017-04-27 00:22:23 -0700

    Update 375_Guess_Number_Higher_or_Lower_II.h

--- LeetCode/375_Guess_Number_Higher_or_Lower_II.h
+++ LeetCode/375_Guess_Number_Higher_or_Lower_II.h
@@ -1,5 +1,5 @@
 //375_Guess_Number_Higher_or_Lower_II.h
-//26ms  36.27%
+//13ms     78.32%
 
 /*
 Total Accepted: 18655
@@ -19,8 +19,11 @@ However, when you guess a particular number x, and you guess wrong, you pay $x. 
 
 //1..n之中猜数字，猜错要扣掉所猜数字的分值，求猜对至少需要多少分。
 //看的solution，用recursive，其实用DP数组可以更快。
+//DP自己做，"DP[i][j]=min{k+ max(DP[i][k-1],DP[k+1][j])} for k=i..j，共O(n^3)"
 
 
+
+///////////////////////////////// solution 1: recursive //26ms  36.27%
     int DP(int *num, int a, int b, int n){
         if (a >= b) return 0;
         if (*(num+a*n+b) > 0) return *(num+a*n+b);
@@ -41,12 +44,24 @@ However, when you guess a particular number x, and you guess wrong, you pay $x. 
         return DP(*num,1,n,n);
     }
 
+///////////////////////////////// solution 2: DP //13ms     78.32%
 
-int main(){
-    int n;
-    cin>>n;
-    while (n > 0){
-        cout<<getMoneyAmount(n)<<endl;
-        cin>>n;
+    int getMoneyAmount(int n) {
+        int DP[n+2][n+2];
+        memset(DP,0,(n+2)*(n+2)*sizeof(int));
+        //for (int i=1;i<=n;i++)
+        //    DP[i][i] = 0;
+        for (int d = 1;d<n;d++)
+            for (int i=1;i<=n-d;i++){
+                int tmp = 0,j=i+d;
+                DP[i][j] = INT_MAX;
+                for (int k=i;k<=j;k++){
+                    tmp = k + max(DP[i][k-1],DP[k+1][j]);
+                    DP[i][j] = min(DP[i][j],tmp);
+                }
+                //cout<<i<<" "<<j<<" "<<DP[i][j]<<endl;
+            }
+        return DP[1][n];
     }
+
 }
