da5d177725e1539ff739db8ebd89ec4df7c72af2
KaiboLiu <liukaibo87@gmail.com>
2017-05-18 12:36:06 -0700

    regular matching, hard

--- LeetCode/010_Regular_Expression_Matching.h
+++ LeetCode/010_Regular_Expression_Matching.h
@@ -1,8 +1,41 @@
 //010_Regular_Expression_Matching.h
 //9ms	91.34%
+//445 / 445 test cases passed.
+
+/*
+Total Accepted: 133552
+Total Submissions: 557447
+Instruction: LeetCode 010 - Wildcard Matching - [H]
+Developer: lrushx
+Process Time: May 17, 2017
+*/
 
 
+/*
+Implement regular expression matching with support for '.' and '*'.
 
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+
+Some examples:
+isMatch("aa","a") → false
+isMatch("aa","aa") → true
+isMatch("aaa","aa") → false
+isMatch("aa", "a*") → true
+isMatch("aa", ".*") → true
+isMatch("ab", ".*") → true
+isMatch("aab", "c*a*b") → true
+*/
+
+//字符串匹配，模式串p中可以有.和*，.表示单个字符，c*表示c字符重复0次或多次(也可以.*表示可以有很多个.)，返回是否匹配
+//DP，若s[i]==p[j]或者p[j]=='.'，则f[i][j]=f[i-1][j-1]；若p[j]=='*'，则f[i][j]=f[i][j-2] || f[i][j-1] || (f[i-1][j-1] && s[i] == p[j-1])  || (p[j-1] == '.' && f[i-1][j]);，分别表示c*匹配空串||匹配1个c||2个c||多个c。
+//初始化时p开头的所有c*连续都要和s的第0位匹配。为了处理初始的空匹配空，把f中的下标都+1即可
+//模式匹配时，s中不会有特殊字符*.，p中有*并且*都是跟在别的字符之后，可以有连续?但是没有连续*和第一个*，初始时要考虑
 
 
     bool isMatch(string s, string p) {
