6a46ce0cbbd5e8a1dc85a1b0fb627913401e658e
Kaibo Liu <liukaibo87@gmail.com>
2017-05-21 19:53:06 -0700

    Update 594_Longest_Harmonious_Subsequence.h

--- LeetCode/594_Longest_Harmonious_Subsequence.h
+++ LeetCode/594_Longest_Harmonious_Subsequence.h
@@ -2,7 +2,6 @@
 //89ms
 //LeetCode Contest 33
 
-
 /*
 Total Accepted: 1586
 Total Submissions: 4662
@@ -11,19 +10,15 @@ Developer: lrushx
 Process Time: May 20, 2017
 */
 
-
 /*
 We define a harmonious array is an array where the difference between its maximum value and its minimum value is exactly 1.
-
 Now, given an integer array, you need to find the length of its longest harmonious subsequence among all its possible subsequences.
-
 Example 1:
 Input: [1,3,2,2,5,2,3,7]
 Output: 5
 Explanation: The longest harmonious subsequence is [3,2,2,2,3].
 Note: The length of the input array will not exceed 20,000.
 */
-
 
 //给一个数列，求其中最大最小差值正好为1的不连续子序列最大长度
 //用unordered_map做hash table，给每个数列元素计数，然后遍历元素k，若k+1也存在，返回max(m[k]+m[k+1])
@@ -45,6 +40,5 @@ Note: The length of the input array will not exceed 20,000.
 	            if (tmp > res) res = tmp;
 	        }
 	    }
-	    return res;
-        
+	    return res;        
     }
