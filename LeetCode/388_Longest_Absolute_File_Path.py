081c348ed133d7da1020ef310f49ee3dd74f492f
Kaibo Liu <liukaibo87@gmail.com>
2017-06-10 01:30:23 -0700

    Create 388_Longest_Absolute_File_Path.py

--- LeetCode/388_Longest_Absolute_File_Path.py
+++ LeetCode/388_Longest_Absolute_File_Path.py
@@ -1,15 +1,15 @@
-//388_Longest_Absolute_File_Path.py
-//35ms	98.40%
+#388_Longest_Absolute_File_Path.py
+#35ms	98.40%
 
-/*
+'''
 Total Accepted: 28550
 Total Submissions: 79276
 Instruction: LeetCode 388 - Longest Absolute File Path - [M]
 Developer: lrushx
 Process Time: June 08, 2017
-*/
+'''
 
-/*
+'''
 Suppose we abstract our file system by a string in the following manner:
 
 The string "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext" represents:
@@ -39,10 +39,10 @@ Note:
 The name of a file contains at least a . and an extension.
 The name of a directory or sub-directory will not contain a ..
 Time complexity required: O(n) where n is the size of the input string.
-*/
+'''
 
-//给一个字符串代表文件系统，\n分割文件(文件夹)，\t个数代表路径层级，有后缀的为文件，求字符路径最长的文件长度
-//在字符串最后加上一个\n，然后循环查找\n，记为end，起始为start，在start处用while往下查找\t个数确定层级i，记下该层长度l[i]=end-start+l[i-1]，i==0单独处理，判断'.'是否在[start,end)区间中，若是则按最大值更新答案，然后start=end+1
+#给一个字符串代表文件系统，\n分割文件(文件夹)，\t个数代表路径层级，有后缀的为文件，求字符路径最长的文件长度
+#在字符串最后加上一个\n，然后循环查找\n，记为end，起始为start，在start处用while往下查找\t个数确定层级i，记下该层长度l[i]=end-start+l[i-1]，i==0单独处理，判断'.'是否在[start,end)区间中，若是则按最大值更新答案，然后start=end+1
 
     def lengthLongestPath(self, input):
         """
