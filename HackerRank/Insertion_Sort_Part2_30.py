726ea6505b39635b050bc10f1a22d64c8234c2e1
Kaibo Liu <liukaibo87@gmail.com>
2018-03-28 18:29:05 -0700

    update

---
+++ HackerRank/Insertion_Sort_Part2_30.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+'''
+Instruction: HackerRank - Insertion_Sort_Part2_30.py - [E]
+Success Rate: %
+Max Score: 30
+Difficulty: Easy 
+Submitted 76121 times
+Developer: lrushx
+Process Time: Mar 28, 2018
+https://www.hackerrank.com/challenges/insertionsort2/problem
+'''
+
+
+import sys
+
+def insertionSort2(n, arr):
+    # Complete this function
+    for i in range(1,n):
+        for j in range(i):
+            if arr[i] < arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+        print(' '.join(map(str,arr)))
+        
+        
+if __name__ == "__main__":
+    n = int(input().strip())
+    arr = list(map(int, input().strip().split(' ')))
+    insertionSort2(n, arr)
